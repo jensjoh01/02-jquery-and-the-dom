a321f34c66aa9505662f9d53fdf5e31bd4f04adb
mstclair <stclairm@spu.edu>
2017-08-15 16:07:41 -0700

    all done

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,7 +3,7 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
+  // DONE: Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
@@ -15,10 +15,10 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  $($newArticle).removeClass('.template');
-  // $($newArticle).addClass(this.title); //should give new class or something to do with show, we also c
 
-  /* TODO: This cloned article still has a class of template.
+  $newArticle.removeClass('template'); //should give new class or something to do with show, we also c
+
+  /* DONE: This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
@@ -26,15 +26,13 @@ Article.prototype.toHtml = function() {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  $('h1').text(this.title);
-
-  $('address').text(this.author);
-  $('a').attr('href', this.authorUrl);
-  $('time').text(this.publishedOn);
-  $('section').text(this.body);
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.article-body').html(this.body);
 
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest
   of the current template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
